51453849f157a0abeca08fe8bb196213a0266a6d
cyrilggrenoble-cmyk <cyril.g.grenoble@gmail.com>
2026-01-20 13:47:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -206,7 +206,8 @@ btnBackHome.addEventListener("click", () => show(viewHome));
 
 btnExport.addEventListener("click", () => {
   const csv = buildCSV(state.collectes);
-  const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+  const BOM = "\uFEFF";
+const blob = new Blob([BOM + csv], { type: "text/csv;charset=utf-8;" });
   const url = URL.createObjectURL(blob);
   const a = document.createElement("a");
   a.href = url;
